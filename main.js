9b93e1a7983058ceac37a51d99ad6671d629317b
Weina Jin <kiwi@Weinas-MacBook-Pro.local>
2015-02-06 14:15:04 -0800

    clear design leaving only one we chat sharing bug

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 var answer=[];
 var count='';
+var result='';
+var shareMeta;
 $(document).ready(function(){
-	$('.house, .foreman, .cuddy, .cameron, .wilson, .chase, .app, .imgShare').hide();
+	$('.house, .foreman, .cuddy, .cameron, .wilson, .chase, .app').hide();
 	$( "form" ).submit(function( event ) {
-    answer=[$('input[name=t0]:checked').val(), 
-    $('input[name=t2]:checked').val(), $('input[name=t3]:checked').val(), 
-    $('input[name=t4]:checked').val(), $('input[name=t5]:checked').val(), 
-    $('input[name=t6]:checked').val()];
-    event.preventDefault();
-    for (var i=0; i<answer.length; i++)
-    {
-    	if (typeof answer[i]!='undefined'){
-	    	var a= answer[i].toString();
-	    	count += a;
-    	};
-	}
-	var result= frequency(count);
-	console.log(result);
-	showChar(result);
-	$('#questions').hide();
-	$("body").animate({ scrollTop: 0 }, "fast");
-	if(req.headers['user-agent'].indexOf('MicroMessenger') !== -1){
-	  //we are in wechat browser
-	    $('.imgShare').show();
-	}
+	    answer=[$('input[name=t0]:checked').val(), 
+			    $('input[name=t2]:checked').val(), $('input[name=t3]:checked').val(), 
+			    $('input[name=t4]:checked').val(), $('input[name=t5]:checked').val(), 
+			    $('input[name=t6]:checked').val()];
+	    event.preventDefault();
+	    for (var i=0; i<answer.length; i++)
+	    {
+	    	if (typeof answer[i]!='undefined'){
+		    	var a= answer[i].toString();
+		    	count += a;
+	    	};
+		}
+		var result= frequency(count);
+		showChar(result);
+		$('#questions').hide();
+		$("body").animate({ scrollTop: 0 }, "fast");
+		if(navigator.userAgent.indexOf('MicroMessenger') !== -1){
+		  //we are in wechat browser
+		    $('#housepoint').show();
+		}
 	});
 });
 
@@ -79,74 +80,97 @@ var frequency = function(string){
 
 
 //微信分享
-switch (result){
-	case 'h':
-		var shareMeta = {
-			img_url: "house.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. House，快来测下你最像哪个角色吧！",
-			appid: ""
-		};
-		break;
-	case 'f':
-		var shareMeta = {
-			img_url: "foreman.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. Foreman，快来测下你最像哪个角色吧！",
-			appid: ""
-		};
-		break;
-	case 'u':
-		var shareMeta = {
-			img_url: "cuddy.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. Cuddy，快来测下你最像哪个角色吧！",
-			appid: ""
-		};
-		break;
-	case 'a':
-		var shareMeta = {
-			img_url: "cameron.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. Cameron，快来测下你最像哪个角色吧！",
-			appid: ""
-		}; 
- 		break;
-	case 'w':
-		var shareMeta = {
-			img_url: "wilson.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. Wilson，快来测下你最像哪个角色吧！",
-			appid: ""
-		}; 
- 		break;
-	case 'c':
-		var shareMeta = {
-			img_url: "chase.jpg",
-			image_width: 100,
-			image_height: 100,
-			link: 'index.html',
-			title: "测测你是《豪斯医生》里的哪个角色？",
-			desc: "我最像《豪斯医生》里的Dr. Chase，快来测下你最像哪个角色吧！",
-			appid: ""
-		}; 
- 		break;									
+
+var sharediffResult=function(result){
+	switch (result){
+		case 'h':
+			shareMeta = {
+				img_url: "http://weina.me/house/house.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. House，快来测下你最像哪个角色吧！",
+				appid: ""
+			};
+			break;
+
+		case 'f':
+			shareMeta = {
+				img_url: "http://weina.me/house/foreman.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. Foreman，快来测下你最像哪个角色吧！",
+				appid: ""
+			};
+			break;
+
+		case 'u':
+			shareMeta = {
+				img_url: "http://weina.me/house/cuddy.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. Cuddy，快来测下你最像哪个角色吧！",
+				appid: ""
+			};
+			break;
+
+		case 'a':
+			shareMeta = {
+				img_url: "http://weina.me/house/cameron.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. Cameron，快来测下你最像哪个角色吧！",
+				appid: ""
+			}; 
+	 		break;
+
+		case 'w':
+			shareMeta = {
+				img_url: "http://weina.me/house/wilson.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. Wilson，快来测下你最像哪个角色吧！",
+				appid: ""
+			}; 
+	 		break;
+
+		case 'c':
+			shareMeta = {
+				img_url: "http://weina.me/house/chase.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "测测你是《豪斯医生》里的哪个角色",
+				desc: "我最像《豪斯医生》里的Dr. Chase，快来测下你最像哪个角色吧！",
+				appid: ""
+			}; 
+	 		break;	
+
+	 	default:
+			shareMeta = {
+				img_url: "http://weina.me/house/background.jpg",
+				image_width: 100,
+				image_height: 100,
+				link: 'http://weina.me/house/index.html',
+				title: "你是《豪斯医生》里的谁？",
+				desc: "快来测测你最像《豪斯医生》里的哪个角色吧！",
+				appid: ""
+			}; 
+	};
+	return shareMeta;
 }
+
+shareMeta=sharediffResult(result);
+console.log(shareMeta);
 
 document.addEventListener('WeixinJSBridgeReady', function () {
 	WeixinJSBridge.on('menu:share:appmessage', function(){
